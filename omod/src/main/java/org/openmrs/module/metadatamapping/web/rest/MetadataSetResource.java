252c3bb1fba72cddaf6c56d74cf1dafc547d8f9a
dkayiwa <kayiwadaniel@gmail.com>
2020-07-20 14:48:28 +0300

    MAP-8 adding support for up to platform 2.4

--- omod/src/main/java/org/openmrs/module/metadatamapping/web/rest/MetadataSetResource.java
+++ omod/src/main/java/org/openmrs/module/metadatamapping/web/rest/MetadataSetResource.java
@@ -22,7 +22,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Resource(name = RestConstants.VERSION_1 + MetadataMappingRestController.METADATA_MAPPING_REST_NAMESPACE + "/metadataset", supportedClass = MetadataSet.class, supportedOpenmrsVersions = {
-        "1.9.*", "1.10.*", "1.11.*", "1.12.*", "2.0.*" })
+        "1.9.*", "1.10.*", "1.11.*", "1.12.*", "2.0.*", "2.1.*", "2.2.*", "2.3.*", "2.4.*" })
 public class MetadataSetResource extends MetadataDelegatingCrudResource<MetadataSet> {
 	
 	@Override
